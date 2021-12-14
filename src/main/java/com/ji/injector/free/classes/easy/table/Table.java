f57e9622e665c9af84f7bc61f81c2ad2161b4cc3
Luca Coraci <luca.coraci@gmail.com>
2021-12-14 02:35:48 +0100

    Table excercise

---
+++ src/main/java/com/ji/injector/free/classes/easy/table/Table.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ji.injector.free.classes.easy.table;
+
+/**
+ *
+ * Inserire come attributi privati una stringa per il 'name' del tavolo e due 
+ * interi, uno per la 'width' e l'altro per la 'height'. 
+ * Si crei (senza usare le shortcut) il costruttore vuoto, uno con tutti gli
+ * argomenti e uno con soli 'width' e 'height'.
+ * Si crei un metodo pubblico 'area' che calcoli l'area del tavolo. 
+ * Si crei un metodo changeName, che cambi il 'name', assicurandosi che se 
+ * il parametro si prova a settare null, il 'name' sia "INVALID". 
+ * @author sommovir
+ */
+public class Table {
+    
+}
---
+++ src/main/java/com/ji/injector/free/classes/easy/table/TableMain.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ji.injector.free.classes.easy.table;
+
+/**
+ *
+ * @author sommovir
+ */
+public class TableMain {
+    
+    //Usare questo main per gli eventuali test
+    public static void main(String[] args) {
+        
+    }
+    
+}
