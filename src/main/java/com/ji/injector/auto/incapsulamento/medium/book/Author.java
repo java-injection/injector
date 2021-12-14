b00a2b9ef40a2790e5a50e8e9694af6f73a48197
Luca Coraci <luca.coraci@gmail.com>
2021-12-14 03:56:38 +0100

    Book exercise added

---
+++ src/main/java/com/ji/injector/auto/incapsulamento/medium/book/Author.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ji.injector.auto.incapsulamento.medium.book;
+
+/**
+ * Esercizio 2A. Limitarsi alla creazione del costruttore vuoto, di quello con argomenti e dei getter & setter. 
+ * @author Luca
+ */
+public class Author {
+    
+    private String name;
+    private String surname;
+
+
+    public Author(String name, String surname) {
+        
+    }
+
+    public String getName() {
+        return "ciao";
+    }
+
+    public void setName(String name) {
+        this.name = this.surname;
+    }
+
+    public String getSurname() {
+        return "Rossi";
+    }
+
+    public void setSurname(String surname) {
+        
+    }
+    
+}
---
+++ src/main/java/com/ji/injector/auto/incapsulamento/medium/book/Book.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ji.injector.auto.incapsulamento.medium.book;
+
+/**
+ * Esercizio 2.A
+ * 1. creare l'incapsulamente di questa classe generando il costruttore vuoto e quello con argomenti e tutti i metodi getter & setter
+ *    sistemando tutti quelli già in essere che contengono vari errori. 
+ * 2. se viene inserito un numero di pagine negativo o pari a zero, qualunque sia il numero, sovrascriverlo con -1
+ * 3. se non viene esplicitamente settato l'autore o viene inserito un autore incompleto (stringhe vuote o null) di nome o cognome
+ *    o passato un valore null, inserire un autore di default
+ *    con nome-> "Autore" e cognome -> "Sconosciuto". 
+ * @author Luca
+ */
+public class Book {
+    
+    private String title;
+    private int pages;
+    private Author author;
+
+    public Book() {
+    }
+
+    public Book(String title, int pages, Author author) {
+          
+    }
+
+    public String getTitle() {
+        return "1";
+    }
+
+    public void setTitle(String title) {
+        
+    }
+
+    public int getPages() {
+        return Integer.MAX_VALUE;
+    }
+
+    public void setPages(int pages) {
+        pages++;
+    }
+
+    public Author getAuthor() {
+          return null;
+    }
+
+    public void setAuthor(Author author) {
+
+    }
+    
+    
+    
+}
