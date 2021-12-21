c482d083906426fa6b90cf888f529dd0bef3e79f
Luca Coraci <luca.coraci@gmail.com>
2021-12-21 22:30:20 +0100

    solution Table

--- src/main/java/com/ji/injector/exercise/classes/easy/table/Table.java
+++ src/main/java/com/ji/injector/exercise/classes/easy/table/Table.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.ji.injector.exercise.classes.easy.table;
 
 import com.ji.injector.headquarter.Exercise;
@@ -9,18 +5,50 @@ import com.ji.injector.headquarter.ExerciseType;
 import com.ji.injector.headquarter.InProgress;
 
 /**
- * F.1-TABLE
- * Inserire come attributi privati una stringa per il 'name' del tavolo e due 
- * interi, uno per la 'width' e l'altro per la 'height'. 
- * Si crei (senza usare le shortcut) il costruttore vuoto, uno con tutti gli
- * argomenti e uno con soli 'width' e 'height'.
- * Si crei un metodo pubblico 'area' che calcoli l'area del tavolo. 
- * Si crei un metodo changeName, che cambi il 'name', assicurandosi che se 
- * il parametro si prova a settare null, il 'name' sia "INVALID". 
+ * F.1-TABLE Inserire come attributi privati una stringa per il 'name' del
+ * tavolo e due interi, uno per la 'width' e l'altro per la 'height'. Si crei
+ * (senza usare le shortcut) il costruttore vuoto, uno con tutti gli argomenti
+ * (nell'ordine: name, width e height) e uno con soli 'width' e 'height'. Si
+ * crei un metodo pubblico 'area' che calcoli l'area del tavolo restituendo
+ * semplicemente il prodotto di width * height. Si crei un metodo 'changeName',
+ * che permetta, da fuori, di cambiare l'attributo'name', assicurandosi che se
+ * il parametro si prova a settare null, il 'name' venga sovrascritto con
+ * "INVALID".
+ *
  * @author sommovir
  */
 @InProgress
-@Exercise(name = "F.1-TABLE", type = ExerciseType.FREE)
+@Exercise(name = "F.1-TABLE", type = ExerciseType.FREE, testEnabled = true)
 public class Table {
+
+    private String name;
+    private int width;
+    private int height;
+
+    public Table() {
+    }
+
+    public Table(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Table(String name, int width, int height) {
+        this.name = name;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int area() {
+        return this.width * this.height;
+    }
+
+    public void changeName(String ciao) {
+        if (ciao == null) {
+            this.name = "INVALID";
+        } else {
+            this.name = ciao;
+        }
+    }
 
 }
