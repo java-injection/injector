f49a7f9f31ca33eb6f716ab183e030c57614fc71
Luca Coraci <luca.coraci@gmail.com>
2021-12-21 22:31:07 +0100

    mini refactor table annotations

--- src/main/java/com/ji/injector/exercise/classes/easy/table/Table.java
+++ src/main/java/com/ji/injector/exercise/classes/easy/table/Table.java
@@ -9,18 +9,20 @@ import com.ji.injector.headquarter.ExerciseType;
 import com.ji.injector.headquarter.InProgress;
 
 /**
- * F.1-TABLE
- * Inserire come attributi privati una stringa per il 'name' del tavolo e due 
- * interi, uno per la 'width' e l'altro per la 'height'. 
- * Si crei (senza usare le shortcut) il costruttore vuoto, uno con tutti gli
- * argomenti e uno con soli 'width' e 'height'.
- * Si crei un metodo pubblico 'area' che calcoli l'area del tavolo. 
- * Si crei un metodo changeName, che cambi il 'name', assicurandosi che se 
- * il parametro si prova a settare null, il 'name' sia "INVALID". 
+ * F.1-TABLE Inserire come attributi privati una stringa per il 'name' del
+ * tavolo e due interi, uno per la 'width' e l'altro per la 'height'. Si crei
+ * (senza usare le shortcut) il costruttore vuoto, uno con tutti gli argomenti
+ * (nell'ordine: name, width e height) e uno con soli 'width' e 'height'. Si
+ * crei un metodo pubblico 'area' che calcoli l'area del tavolo restituendo
+ * semplicemente il prodotto di width * height. Si crei un metodo 'changeName',
+ * che permetta, da fuori, di cambiare l'attributo'name', assicurandosi che se
+ * il parametro si prova a settare null, il 'name' venga sovrascritto con
+ * "INVALID".
+ *
  * @author sommovir
  */
 @InProgress
-@Exercise(name = "F.1-TABLE", type = ExerciseType.FREE)
+@Exercise(name = "F.1-TABLE", type = ExerciseType.FREE, testEnabled = true)
 public class Table {
 
 }
