4d04b9ba8f1608f76dfb661140e2a9998a26e3e4
Luca Coraci <luca.coraci@gmail.com>
2021-12-22 18:14:18 +0100

    fix score

--- src/test/java/Lesson1AUnitTest.java
+++ src/test/java/Lesson1AUnitTest.java
@@ -29,6 +29,7 @@ public class Lesson1AUnitTest {
     private String result;
     private static float vote = 0;
     private static PasswordManager pm = null;
+    private static final String BREAKLINE = "\n---------------------------------------------------------------------------------------------------";
 
     public Lesson1AUnitTest() {
     }
@@ -72,12 +73,12 @@ public class Lesson1AUnitTest {
     public void test1a1(TestInfo info) {
 
         message = info.getDisplayName();
-        result = "\t\t\t\t[FAIL]";
+        result = "\t\t[FAIL]"+BREAKLINE;
 
         int maxa = pm.getMaxAttempts();
         assertEquals(3, maxa,"Mi aspettavo che il numero massimo di tentativi fosse 3, invece mi hai restituito "+maxa);
 
-        result = "\t\t\t\t[SUCCESS][+5.0]";
+        result = "\t\t[SUCCESS][+5.0]"+BREAKLINE;
         vote += 5.0f;
     }
     
@@ -86,12 +87,12 @@ public class Lesson1AUnitTest {
     public void test1a2(TestInfo info) {
 
         message = info.getDisplayName();
-        result = "\t\t\t[FAIL]";
+        result = "\t[FAIL]"+BREAKLINE;
 
         int wa = pm.getWrongAttempts();
         assertEquals(0, wa,"All'inizio il numero di errori deve essere zero, invece mi dai come valore: "+wa);
 
-        result = "\t\t\t[SUCCESS][+2.0]";
+        result = "\t[SUCCESS][+2.0]"+BREAKLINE;
         vote += 2.0f;
     }
     
@@ -100,19 +101,19 @@ public class Lesson1AUnitTest {
     public void test1a3(TestInfo info) {
 
         message = info.getDisplayName();
-        result = "\t\t\t\t[SKIPPED] (fix 1.A.2, 1)";
+        result = "\t\t[SKIPPED] (fix 1.A.2, 1)";
 
         int wa = pm.getWrongAttempts();
         
         Assumptions.assumeTrue(wa==0);
-        result = "\t\t\t\t[FAIL]";
+        result = "\t\t[FAIL]";
         pm.checkPassword("erro1");
         pm.checkPassword("erro1");
         wa = pm.getWrongAttempts();
         assertEquals(2, wa,"Ho eseguito due tentativi errati ma il conteggio è errato");
 
-        result = "\t\t\t\t[SUCCESS][+6.0]";
-        vote += 6.0f;
+        result = "\t\t[SUCCESS][+3.0]";
+        vote += 3.0f;
     }
     
     @Test
@@ -120,12 +121,12 @@ public class Lesson1AUnitTest {
     public void test1a4(TestInfo info) {
 
         message = info.getDisplayName();
-        result = "\t\t\t[SKIPPED] (fix 1.A.2, 1)";
+        result = "\t[SKIPPED] (fix 1.A.2, 1)";
 
         int wa = pm.getWrongAttempts();
         
         Assumptions.assumeTrue(wa==0);
-        result = "\t\t\t[FAIL]";
+        result = "\t[FAIL]";
         pm.checkPassword("erro1");
         pm.checkPassword("erro1");
         pm.checkPassword("erro12");
@@ -134,7 +135,7 @@ public class Lesson1AUnitTest {
         wa = pm.getWrongAttempts();
         assertEquals(3, wa,"Ho eseguito 6 tentativi errati ma il conteggio è errato, non dovrebbe superare le 3 prove invece.. : "+wa);
 
-        result = "\t\t\t[SUCCESS][+5.0]";
+        result = "\t[SUCCESS][+5.0]";
         vote += 5.0f;
     }
     
@@ -143,18 +144,18 @@ public class Lesson1AUnitTest {
     public void test1a5(TestInfo info) {
 
         message = info.getDisplayName();
-        result = "\t\t\t[SKIPPED] (fix 1.A.2, 1)";
+        result = "\t[SKIPPED] (fix 1.A.2, 1)";
 
         int wa = pm.getWrongAttempts();
         
         Assumptions.assumeTrue(wa==0);
-        result = "\t\t\t[FAIL]";
+        result = "\t[FAIL]";
         String value = pm.checkPassword("antisgamo");
         wa = pm.getWrongAttempts();
         assertEquals(0, wa,"Ho subito inserito la password corretta e mi aspettavo "+wa);
         assertEquals("CORRECT", value,"Ho subito inserito la password corretta ma non mi ha ritornato il giusto valore");
 
-        result = "\t\t\t[SUCCESS][+3.0]";
+        result = "\t[SUCCESS][+3.0]";
         vote += 3.0f;
     }
     
@@ -163,20 +164,20 @@ public class Lesson1AUnitTest {
     public void test1a6(TestInfo info) {
 
         message = info.getDisplayName();
-        result = "\t\t\t[SKIPPED] (fix 1.A.2, 1)";
+        result = "\t[SKIPPED] (fix 1.A.2, 1)";
 
         int wa = pm.getWrongAttempts();
         
         Assumptions.assumeTrue(wa==0);
-        result = "\t\t\t[FAIL]";
+        result = "\t[FAIL]";
         pm.checkPassword("alfonso");
         pm.checkPassword("minki");
         String value = pm.checkPassword("antisgamo");
         wa = pm.getWrongAttempts();
         assertEquals(0, wa,"Ho inserito la password corretta dopo due errori ma il contatore di passwrod segna ancora: "+wa+".  Mi hai molto deluso.");
         assertEquals("CORRECT", value,"Ho inserito la password corretta ma non mi ha ritornato il giusto valore");
-        result = "\t\t\t[SUCCESS][+5.0]";
-        vote += 5.0f;
+        result = "\t[SUCCESS][+4.0]";
+        vote += 4.0f;
     }
     
     @Test
@@ -184,12 +185,12 @@ public class Lesson1AUnitTest {
     public void test1a7(TestInfo info) {
 
         message = info.getDisplayName();
-        result = "\t\t\t\t[SKIPPED] (fix 1.A.2, 1)";
+        result = "\t\t[SKIPPED] (fix 1.A.2, 1)";
 
         int wa = pm.getWrongAttempts();
         
         Assumptions.assumeTrue(wa==0);
-        result = "\t\t\t\t[FAIL]";
+        result = "\t\t[FAIL]";
         String checkValue1 = pm.checkPassword("alfonso");
         String checkValue2 = pm.checkPassword("minki");
         String checkValue3 = pm.checkPassword("antisgamo");
@@ -198,7 +199,7 @@ public class Lesson1AUnitTest {
         assertEquals("ERROR", checkValue2,"ho inserito una password errata ma non hai restituito il giusto valore di ritorno");
         assertEquals("CORRECT", checkValue3,"ho inserito una password giusta ma non hai restituito il giusto valore di ritorno");
 
-        result = "\t\t\t\t[SUCCESS][+5.0]";
+        result = "\t\t[SUCCESS][+5.0]";
         vote += 5.0f;
     }
     
@@ -208,12 +209,12 @@ public class Lesson1AUnitTest {
     public void test1a8(TestInfo info) {
 
         message = info.getDisplayName();
-        result = "\t\t\t\t[SKIPPED] (fix 1.A.2, 1)";
+        result = "\t\t[SKIPPED] (fix 1.A.2, 1)";
 
         int wa = pm.getWrongAttempts();
         
         Assumptions.assumeTrue(wa==0);
-        result = "\t\t\t\t[FAIL]";
+        result = "\t\t[FAIL]";
         pm.checkPassword("alfonso");
         pm.checkPassword("minki");
         pm.checkPassword("minki2");
@@ -222,8 +223,8 @@ public class Lesson1AUnitTest {
         
         assertEquals("BLOCKED", checkValue,"ho superato il numero massimo di errori e mi aspettavo il valore di ritorno BLOCKED.. e invece..:"+checkValue);
 
-        result = "\t\t\t\t[SUCCESS][+2.0]";
-        vote += 2.0f;
+        result = "\t\t[SUCCESS][+4.0]";
+        vote += 4.0f;
     }
     
 
